4a88f49e2d559c76a721c071ebb7a825751ca697
Neeraja R Nair <144925949+Neeraja2005R@users.noreply.github.com>
2025-08-14 22:15:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let section = document.querySelectorAll('secton');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(link =>{
+                links.classList.remove('active');
+                document.querySelectorAll('header nav a [href*='+id+']').classList.add('active')
+            })
+        }
+    })
+}
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('bx bx-menu');
+    navLinks.classList.toggle('active');
+}
